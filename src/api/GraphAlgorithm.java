8a6ec46eefbdac43bb705dff406a8a5e14b05c31
shlomoPearl <shlomoper94@gmail.com>
2021-12-08 19:52:40 +0200

    8/12

--- src/api/GraphAlgorithm.java
+++ src/api/GraphAlgorithm.java
@@ -1,12 +1,17 @@
 package api;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import org.json.JSONObject;
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+//import com.google.gson.JsonObject;
+//import org.json.JSONObject;
+//import com.google.gson.JsonObject;
+//import org.json.simple.JSONObject;
 
 import java.io.FileWriter;
 import java.io.IOException;
+
+import org.json.simple.JSONObject;
+
 import java.util.*;
 
 
@@ -38,7 +43,7 @@ public class GraphAlgorithm implements DirectedWeightedGraphAlgorithms {
 
     private boolean dfs(DWGraph g, Node n) {
         n.setTag(1);
-        if(g.edgeIter(n.getKey())!=null) {
+        if (g.edgeIter(n.getKey()) != null) {
             Iterator<EdgeData> edgeIter = g.edgeIter(n.getKey());
             while (edgeIter.hasNext()) {
                 Node next = (Node) g.getNode(edgeIter.next().getDest());
@@ -114,7 +119,7 @@ public class GraphAlgorithm implements DirectedWeightedGraphAlgorithms {
 
     @Override
     public List<NodeData> shortestPath(int src, int dest) {
-        if (src == dest)return null;
+        if (src == dest) return null;
         LinkedList<NodeData> ans = new LinkedList<>();
 
         HashMap<Integer, Node> unCheckedNode = new HashMap<>();
@@ -150,72 +155,158 @@ public class GraphAlgorithm implements DirectedWeightedGraphAlgorithms {
                         }
                     }
                 }
-            }else continue;
+            } else continue;
         }
         return (!ans.isEmpty()) ? ans : null;
     }
 
+
     @Override
     public NodeData center() {
-        return null;
+        double[] ans = new double[2];
+        ans[1] = Double.MAX_VALUE;
+        double count;
+        Iterator<NodeData> nodeIter1 = g.nodeIter();
+        while (nodeIter1.hasNext()) {
+            Node current1 = (Node) nodeIter1.next();
+            shortestPath(current1.getKey());
+            Iterator<NodeData> nodeIter2 = g.nodeIter();
+            count = 0;
+            while (nodeIter2.hasNext()) {
+                Node current2 = (Node) nodeIter2.next();
+                count += current2.getInWeight();
+            }
+            if (count < ans[1]) {
+                ans[0] = current1.getKey();
+                ans[1] = count;
+            }
+        }
+        return g.getNode((int) ans[0]);
     }
 
-    private String tuple(int src, int dest) {
-        return "(" + src + ", " + dest + ")"; //+ ", "+w
+    private void shortestPath(int src) {
+        HashMap<Integer, Node> unCheckedNode = new HashMap<>();
+        Iterator<NodeData> nodeIter = g.nodeIter();
+        while (nodeIter.hasNext()) {
+            Node current = (Node) nodeIter.next();
+            unCheckedNode.put(current.getKey(), current);
+        }
+        initialMax(unCheckedNode, src);
+
+        while (!unCheckedNode.isEmpty()) {
+            Node currentNode = minWeight(unCheckedNode);
+            unCheckedNode.remove(currentNode.getKey());
+            if (g.edgeIter(currentNode.getKey()) != null) {
+                Iterator<EdgeData> edgeIterator = g.edgeIter(currentNode.getKey());
+                while (edgeIterator.hasNext()) {
+                    Edge currentEdge = (Edge) edgeIterator.next();
+                    Node nextNode = (Node) g.getNode(currentEdge.getDest());
+                    Node prevNode = (Node) g.getNode(currentEdge.getSrc());
+                    if (currentEdge.getWeight() + currentNode.getInWeight() < nextNode.getInWeight()) {
+                        nextNode.setInWeight(currentEdge.getWeight() + currentNode.getInWeight());
+                        nextNode.setKeyPrevNode(currentNode.getKey());
+                    }
+                }
+            } else continue;
+        }
+    }
+
+    private double[] chooseStartNode(List<NodeData> unCheckNode) {
+        double[] temp = new double[3];
+        temp[2] = Double.MAX_VALUE;
+        for (int i = 0; i < unCheckNode.size(); i++) {
+            shortestPath(unCheckNode.get(i).getKey());
+            for (int j = 0; j < unCheckNode.size(); j++) {
+                if (i == j) continue;
+                if (((Node) (unCheckNode.get(j))).getInWeight() < temp[2]) {
+                    temp[0] = unCheckNode.get(i).getKey();
+                    temp[1] = unCheckNode.get(j).getKey();
+                    temp[2] = ((Node) (unCheckNode.get(j))).getInWeight();
+                }
+            }
+        }
+        return temp;
+    }
+
+    private double[] minWeight(List<NodeData> unCheckNode, int src) {
+        shortestPath(src);
+        double[] temp2 = new double[2];
+        temp2[1] = Double.MAX_VALUE;
+        for (int i = 0; i < unCheckNode.size(); i++) {
+            if (((Node) (unCheckNode.get(i))).getInWeight() < temp2[1]) {
+                temp2[0] = unCheckNode.get(i).getKey();
+                temp2[1] = ((Node) (unCheckNode.get(i))).getInWeight();
+            }
+        }
+        return temp2;
+    }
+
+    private void removeDupi(List<NodeData> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).getKey() == list.get(i + 1).getKey()) {
+                list.remove(i);
+            }
+        }
     }
 
 
     @Override
     public List<NodeData> tsp(List<NodeData> cities) {
-        HashMap<String,Double> path = new HashMap<>();
+        List<NodeData> ans = new ArrayList<>();
+        List<NodeData> unCheckNode = new LinkedList<>();
+        unCheckNode.addAll(cities);
 
-        for (int i = 0; i < cities.size(); i++) {
-
+        double[] temp = chooseStartNode(unCheckNode);
+        int s, d;
+        s = (int) temp[0];
+        d = (int) temp[1];
+        ans.addAll(shortestPath(s, d));
+        for (int i = 0; i < ans.size(); i++) {
+            if (unCheckNode.contains(ans.get(i))) {
+                unCheckNode.remove(ans.get(i));
+            }
         }
+        while (!unCheckNode.isEmpty()) {
+            double[] minW = minWeight(unCheckNode, d);
+            ans.addAll((shortestPath(d, (int) minW[0])));
+            d = (int) minW[0];
+            for (int i = 0; i < ans.size(); i++) {
+                if (unCheckNode.contains(ans.get(i))) {
+                    unCheckNode.remove(ans.get(i));
+                }
+            }
+        }
+        removeDupi(ans);
+        if (ans != null) return ans;
         return null;
     }
 
     @Override
     public boolean save(String file) {
-        JsonObject jsonObject = new JsonObject();
-        Iterator<EdgeData> edgeIter = g.edgeIter();
-        Iterator<NodeData> nodeIter = g.nodeIter();
-        while (edgeIter.hasNext()){
-            Edge current = (Edge) edgeIter.next();
-            jsonObject.addProperty("src",current.getSrc());
-            jsonObject.addProperty("w",current.getWeight());
-            jsonObject.addProperty("dest",current.getDest());
+        JSONObject jsonObject = new JSONObject();
+        try {
+            FileWriter save = new FileWriter(file);
+            Iterator<EdgeData> edgeIter = g.edgeIter();
+            Iterator<NodeData> nodeIter = g.nodeIter();
+            jsonObject.put("Edges",'[');
+            while (edgeIter.hasNext()) {
+                Edge current = (Edge) edgeIter.next();
+                jsonObject.put("src", current.getSrc());
+                jsonObject.put("w", current.getWeight());
+                jsonObject.put("dest", current.getDest());
+                save.write(jsonObject.toJSONString());
+            }
+            jsonObject.put("Nodes",'[');
+            while (nodeIter.hasNext()) {
+                Node current = (Node) nodeIter.next();
+                jsonObject.put("pos", "" + current.getLocation().x() + "," + current.getLocation().y() + "," + current.getLocation().z());
+                jsonObject.put("id", current.getKey());
+                save.write(jsonObject.toJSONString());
+            }
+            save.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        while (nodeIter.hasNext()){
-            Node current = (Node) nodeIter.next();
-            jsonObject.addProperty("pos",""+current.getLocation().x()+","+current.getLocation().y()+","+current.getLocation().z());
-            jsonObject.addProperty("id",current.getKey());
-        }
-
-        GsonBuilder  builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-        Gson gson = builder.create();
-        gson.toJson(jsonObject);
-
-        //gson.toJson(jsonObject);
-
-        //System.out.println(jsonObject);
-//        try{
-//            FileWriter save = new FileWriter(file);
-//            save.write(jsonObject.getAsString());
-//            save.close();
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        Gson gson = new Gson();
-//        file = gson.toJson(g.getNode(2));
-//        System.out.println(file);
-//      //  file = gson.toJson(g.getNode(2));
-////        try {
-////            gson.toJson(g, new FileWriter(file));
-////        } catch (IOException e) {
-////            e.printStackTrace();
-////        }
         return true;
     }
 
--- src/api/main.java
+++ src/api/main.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class main {
 
     public static void main(String[] args) {
-        DWGraph graph = new DWGraph();
+        DWGraph graph1 = new DWGraph();
+        DWGraph graph2 = new DWGraph();
         Node n1 = new Node(1, new Location(0, 0, 0));
         Node n2 = new Node(2, new Location(1, 1, 0));
         Node n3 = new Node(3, new Location(1, 1, 0));
@@ -16,32 +17,64 @@ public class main {
         Node n5 = new Node(5, new Location(1, 1, 0));
         Node n6 = new Node(6, new Location(1, 1, 0));
 
-        graph.addNode(n1);
-        graph.addNode(n2);
-        graph.addNode(n3);
-        graph.addNode(n4);
-        graph.addNode(n5);
-        graph.addNode(n6);
-        graph.connect(n1.getKey(), n2.getKey(), 2);
-        graph.connect(n1.getKey(), n3.getKey(), 4);
-        graph.connect(n2.getKey(), n4.getKey(), 7);
-        graph.connect(n2.getKey(), n3.getKey(), 1);
-        graph.connect(n4.getKey(), n6.getKey(), 1);
-        graph.connect(n3.getKey(), n5.getKey(), 3);
-        graph.connect(n3.getKey(), n1.getKey(), 3);
-        graph.connect(n5.getKey(), n4.getKey(), 2);
-        graph.connect(n5.getKey(), n6.getKey(), 5);
-        graph.connect(n5.getKey(), n3.getKey(), 5);
-        graph.connect(n6.getKey(), n5.getKey(), 5);
+        graph1.addNode(n1);
+        graph1.addNode(n2);
+        graph1.addNode(n3);
+        graph1.addNode(n4);
+        graph1.addNode(n5);
+        graph1.addNode(n6);
+
+        graph2.addNode(n1);
+        graph2.addNode(n2);
+        graph2.addNode(n3);
+
+        graph1.connect(n1.getKey(), n2.getKey(), 2);
+        graph1.connect(n1.getKey(), n3.getKey(), 4);
+        graph1.connect(n2.getKey(), n4.getKey(), 7);
+        graph1.connect(n2.getKey(), n3.getKey(), 1);
+        graph1.connect(n4.getKey(), n6.getKey(), 1);
+        graph1.connect(n3.getKey(), n5.getKey(), 3);
+        graph1.connect(n3.getKey(), n1.getKey(), 3);
+        graph1.connect(n5.getKey(), n4.getKey(), 7);
+        graph1.connect(n5.getKey(), n6.getKey(), 1);
+        graph1.connect(n5.getKey(), n3.getKey(), 5);
+        graph1.connect(n6.getKey(), n5.getKey(), 5);
+        graph1.connect(n5.getKey(), n6.getKey(), 1);
+        graph1.connect(n6.getKey(), n4.getKey(), 1);
+
+        graph2.connect(n1.getKey(), n2.getKey(), 1);
+        graph2.connect(n1.getKey(), n3.getKey(), 1);
+        graph2.connect(n2.getKey(), n1.getKey(), 5);
+        graph2.connect(n2.getKey(), n3.getKey(), 5);
+        graph2.connect(n3.getKey(), n2.getKey(), 5);
+        graph2.connect(n3.getKey(), n1.getKey(), 5);
 
         GraphAlgorithm g1 =new GraphAlgorithm();
-        g1.init(graph);
-        double d = g1.shortestPathDist(2,4);
+        g1.init(graph1);
+        GraphAlgorithm g2 =new GraphAlgorithm();
+        g2.init(graph2);
+
+        double d = g1.shortestPathDist(2,6);
         LinkedList<NodeData> list= (LinkedList<NodeData>) g1.shortestPath(2,4);
         g1.save("shlomo.json");
         System.out.println(d);
-        System.out.println(list.get(2).getKey());
+        //System.out.println(list.get(2).getKey());
         System.out.println(g1.isConnected());
+        List<NodeData> check = new LinkedList<>();
+        check.add(n2);
+        check.add(n3);
+        check.add(n4);
+        check.add(n5);
+        List<NodeData> travel = g1.tsp(check);
+        for (int i = 0; i < travel.size()-1; i++) {
+            System.out.print(travel.get(i).getKey()+" ->");
+
+        }
+        System.out.print(list.get(list.size()-1).getKey());
+        System.out.println();
+        System.out.println("center g1: "+g1.center().getKey());
+        int a = g2.center().getKey();
+        System.out.println("center g2: "+a);
 
     }
 
