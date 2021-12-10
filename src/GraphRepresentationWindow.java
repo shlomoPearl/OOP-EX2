9f7cbd2bc3b8fbd0ce88c170552d5ec4f25e3d7a
Dovi-Amiram <73857923+Dovi-Amiram@users.noreply.github.com>
2021-12-10 08:22:08 +0200

    comments on functions

--- src/GraphRepresentationWindow.java
+++ src/GraphRepresentationWindow.java
@@ -1,11 +1,12 @@
 import api.GraphAlgorithm;
 
 import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Style;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
-public class GraphRepresentationWindow extends JFrame implements ActionListener {
+public class GraphRepresentationWindow extends JFrame implements ActionListener, MouseListener, MouseMotionListener {
     ParameterWindow p;
 
     MenuItem loadButton = new MenuItem("Load Graph");
@@ -29,9 +30,10 @@ public class GraphRepresentationWindow extends JFrame implements ActionListener 
         addMenu();
     }
 
+
     private void initFrame() {
-//        JFrame frame = new JFrame();
-        this.setTitle("GUI for Graph");
+        this.setLayout(new FlowLayout());
+        this.setTitle("Directed Weighted Graph Representation");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000, 800);
         this.setVisible(true);
@@ -83,73 +85,108 @@ public class GraphRepresentationWindow extends JFrame implements ActionListener 
         algorithmsMenu.add(getEdgeSize);
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == saveButton) {
             p = new ParameterWindow("s", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == loadButton) {
+
+        } else if (e.getSource() == loadButton) {
             p = new ParameterWindow("l", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == addNodeButton) {
+
+        } else if (e.getSource() == addNodeButton) {
             p = new ParameterWindow("AN", this);
             p.setVisible(true);
-        }
 
-        else if (e.getSource() == connectButton) {
+        } else if (e.getSource() == connectButton) {
             p = new ParameterWindow("c", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == shortestPath) {
+
+        } else if (e.getSource() == shortestPath) {
             p = new ParameterWindow("SP", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == shortestPathDist) {
+
+        } else if (e.getSource() == shortestPathDist) {
             p = new ParameterWindow("SPD", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == removeEdgeButton) {
+
+        } else if (e.getSource() == removeEdgeButton) {
             p = new ParameterWindow("RE", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == removeNodeButton) {
+
+        } else if (e.getSource() == removeNodeButton) {
             p = new ParameterWindow("RN", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == tspButton) {
+
+        } else if (e.getSource() == tspButton) {
             p = new ParameterWindow("TSP", this);
             p.setVisible(true);
-        }
-        else if (e.getSource() == isConnected) {
 
-        }
-        else if (e.getSource() == getEdgeSize) {
+        } else if (e.getSource() == isConnected) {
+            JLabel answer = new JLabel(String.valueOf(graph_algo.isConnected()));
+            answer.setFont(new Font("ComicSans", Font.BOLD, 70));
+            answer.setBounds(500, 400, answer.getPreferredSize().width, answer.HEIGHT);
+            this.add(answer);
 
-        }
-        else if (e.getSource() == getNodeSize) {
 
-        }
-        else if (e.getSource() == center) {
+        } else if (e.getSource() == getEdgeSize) {
+
+        } else if (e.getSource() == getNodeSize) {
+
+        } else if (e.getSource() == center) {
 
         }
     }
-    public void save(String path){
-       graph_algo.save(path);
+
+    public void save(String path) {
+        graph_algo.save(path);
         System.out.println(graph_algo.getGraph());
     }
 
-    protected void load(String path){
+    protected void load(String path) {
         graph_algo = new GraphAlgorithm();
         graph_algo.load(path);
         System.out.println(graph_algo.getGraph());
     }
 
+
     public static void main(String[] args) {
         new GraphRepresentationWindow();
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
 
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
 }
